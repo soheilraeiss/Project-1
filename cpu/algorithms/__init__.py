3780b5cac17c0e8c083fa6017c888f9d4b508b3e
Seyed Soheil Raeisossadati <124075445+soheilraeiss@users.noreply.github.com>
2025-12-17 18:01:32 +0330

    Update __init__.py

---
+++ cpu/algorithms/__init__.py
@@ -0,0 +1,6 @@
+from .fcfs import fcfs
+from .sjf import sjf
+from .srtf import srtf
+from .round_robin import round_robin
+from .priority_non_preemptive import priority_non_preemptive
+from .priority_preemptive import priority_preemptive
